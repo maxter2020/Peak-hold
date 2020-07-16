e93ad8fad0eb794863a054732a8e0467725f29bf
Max Booth <max@paulbooth.com>
2020-07-16 13:30:10 +0100

    Added for loop testing

--- Peak HoldTests/Peak_HoldTests.swift
+++ Peak HoldTests/Peak_HoldTests.swift
@@ -69,4 +69,14 @@ class Peak_HoldTests: XCTestCase {
             }
         }
     }
+    
+    func testPeakHoldCalculatorForLoop() {
+        self.measure {
+            for i in 0...9999{
+                let output = PeakHold.peakHoldCalculatorForLoop(data: testData[i], peakHoldIn: peakHold, counterIn: counter, holdTime: holdTime, dropSpeed: dropSpeed)
+                peakHold = output.peakHold
+                counter = output.counter
+            }
+        }
+    }
 }
